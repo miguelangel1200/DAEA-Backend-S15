71ebad89401fbb3e86fdccdf7934c2220c1bafde
Miguel Angel <miguel@gmail.com>
2023-07-07 01:19:22 -0500

    first commit

---
+++ APITest/Controllers/PeopleController.cs
@@ -0,0 +1,29 @@
+﻿using APITest.Response;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APITest.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class PeopleController : Controller
+    {
+        [Authorize]
+        [HttpGet(Name = "Get")]
+        public IEnumerable<PersonResponse> Get()
+        {
+            List<PersonResponse> personas = new List<PersonResponse>();
+
+            for (int i=1; i<100; i++)
+            {
+                PersonResponse persona = new PersonResponse();
+                persona.FirstName = "Persona " + i;
+                persona.LastName = "Apellido " + i;
+
+                personas.Add(persona);
+            }
+
+            return personas;
+        }
+    }
+}
